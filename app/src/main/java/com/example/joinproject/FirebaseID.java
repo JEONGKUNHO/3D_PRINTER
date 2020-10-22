0ea1a9c3e7c8dc0f06d52b7246505f3046736c2c
JEONGKUNHO <dghj6739@gmail.com>
2020-10-22 22:22:00 +0900

    회원가입까지

---
+++ app/src/main/java/com/example/joinproject/FirebaseID.java
@@ -0,0 +1,8 @@
+package com.example.joinproject;
+
+public class FirebaseID {
+    public static String user="user";
+    public static String documentID="documentID";
+    public static String email="email";
+    public static String password="password";
+}
