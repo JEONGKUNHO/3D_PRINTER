4dab282626333af34ca283aedd1ef58c6b8e460d
JEONGKUNHO <dghj6739@gmail.com>
2020-11-11 20:33:57 +0900

    기업목록까지 !!

---
+++ app/src/main/java/com/example/joinproject/Company.java
@@ -0,0 +1,69 @@
+package com.example.joinproject;
+
+public class Company {
+    private int comp_num;
+    private String comp_name;
+    private String comp_boss;
+    private String comp_email;
+    private int comp_star;
+    private String comp_image;
+    private String comp_intro;
+
+    public Company(){}
+
+    public int getComp_num() {
+        return comp_num;
+    }
+
+    public void setComp_num(int comp_num) {
+        this.comp_num = comp_num;
+    }
+
+    public String getComp_name() {
+        return comp_name;
+    }
+
+    public void setComp_name(String comp_name) {
+        this.comp_name = comp_name;
+    }
+
+    public String getComp_boss() {
+        return comp_boss;
+    }
+
+    public void setComp_boss(String comp_boss) {
+        this.comp_boss = comp_boss;
+    }
+
+    public String getComp_email() {
+        return comp_email;
+    }
+
+    public void setComp_email(String comp_email) {
+        this.comp_email = comp_email;
+    }
+
+    public int getComp_star() {
+        return comp_star;
+    }
+
+    public void setComp_star(int comp_star) {
+        this.comp_star = comp_star;
+    }
+
+    public String getComp_image() {
+        return comp_image;
+    }
+
+    public void setComp_image(String comp_image) {
+        this.comp_image = comp_image;
+    }
+
+    public String getComp_intro() {
+        return comp_intro;
+    }
+
+    public void setComp_intro(String comp_intro) {
+        this.comp_intro = comp_intro;
+    }
+}
---
+++ app/src/main/java/com/example/joinproject/CompanyAdapter.java
@@ -0,0 +1,62 @@
+package com.example.joinproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class CompanyAdapter extends RecyclerView.Adapter<CompanyAdapter.CompanyViewHolder> {
+
+    private ArrayList<Company> arrayList;
+    private Context context;
+
+    public CompanyAdapter(ArrayList<Company> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CompanyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.company_item,parent,false);
+        CompanyViewHolder holder=new CompanyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CompanyViewHolder holder, int position) {
+        Glide.with(holder.itemView)
+                .load(arrayList.get(position).getComp_image())
+                .into(holder.comp_image);
+        holder.comp_name.setText(arrayList.get(position).getComp_name());
+        holder.comp_star.setText(String.valueOf(arrayList.get(position).getComp_star()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (arrayList!=null ? arrayList.size() : 0);
+    }
+
+    public class CompanyViewHolder extends RecyclerView.ViewHolder {
+        ImageView comp_image;
+        TextView comp_name;
+        TextView comp_star;
+
+
+        public CompanyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.comp_image=itemView.findViewById(R.id.comp_image);
+            this.comp_name=itemView.findViewById(R.id.comp_name);
+            this.comp_star=itemView.findViewById(R.id.comp_star);
+        }
+    }
+}
